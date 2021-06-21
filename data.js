458c7b01454b156d4413d0483095ce3d379b8d36
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-06-20 23:15:00 -0300

    add colpayop remove fiambreria

--- data.js
+++ data.js
@@ -118,16 +118,16 @@ const stores = [
 
     },
     {
-    name:"LA TRANQUERA GOURMET",
-    category:"FIAMBRERIA",
-    adress:"Av Pedro Goyena 394",
-    contact:"bissonegustavo@hotmail.com",
+    name:"OKAY CABALLITO",
+    category:"RESTAURANTE",
+    adress:"Colpayo 303",
+    contact:"",
     picture:"./assets/stores/FIAMBRE1.jpg",
     cuponClass: "senior",
     facebook:"",
     instagram:"",
-    telephone:"5940-1391",
-    map:'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.1417470249635!2d-58.43479578560833!3d-34.62585808045375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca4eeb759ceb%3A0xf76dc70cf79f7cd9!2sAv.%20Pedro%20Goyena%20394%2C%20C1424BSQ%20CABA%2C%20Argentina!5e0!3m2!1spt-BR!2sbr!4v1614539058147!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
+    telephone:"6000-6400",
+    map:'<iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.4999619698942!2d-58.443606684387724!3d-34.61680388045608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3f9ef4b133%3A0xea9e8379aec22858!2sColpayo%20303%2C%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1spt-BR!2sbr!4v1624241598211!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
     id: 10
     },
     {

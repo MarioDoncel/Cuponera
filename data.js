a9c374194c1f7089befce22e9e4068d9c49cef20
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-06-20 23:18:40 -0300

    Ordem estabelecimentos

--- data.js
+++ data.js
@@ -118,19 +118,6 @@ const stores = [
 
     },
     {
-    name:"OKAY CABALLITO",
-    category:"RESTAURANTE",
-    adress:"Colpayo 303",
-    contact:"",
-    picture:"./assets/stores/COLPAYO1.jpg",
-    cuponClass: "senior",
-    facebook:"",
-    instagram:"",
-    telephone:"6000-6400",
-    map:'<iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.4999619698942!2d-58.443606684387724!3d-34.61680388045608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3f9ef4b133%3A0xea9e8379aec22858!2sColpayo%20303%2C%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1spt-BR!2sbr!4v1624241598211!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
-    id: 10
-    },
-    {
     name:"GRANJA L P",
     category:"GRANJA",
     adress:"Franklin 914",
@@ -246,6 +233,19 @@ const stores = [
     telephone:"3033-2221",
     map:'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.3055773735414!2d-58.448516985608386!3d-34.62171738045486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3bfb5297f3%3A0xba4ba0a7768be97a!2sEspinosa%2068%2C%20C1406CBB%20CABA%2C%20Argentina!5e0!3m2!1spt-BR!2sbr!4v1614539324719!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
     id:18
+    },
+    {
+        name:"OKAY CABALLITO",
+        category:"RESTAURANTE",
+        adress:"Colpayo 303",
+        contact:"",
+        picture:"./assets/stores/COLPAYO1.jpg",
+        cuponClass: "senior",
+        facebook:"",
+        instagram:"",
+        telephone:"6000-6400",
+        map:'<iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.4999619698942!2d-58.443606684387724!3d-34.61680388045608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3f9ef4b133%3A0xea9e8379aec22858!2sColpayo%20303%2C%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1spt-BR!2sbr!4v1624241598211!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
+        id: 10
     },
     {
     name:"ESTUDIO AFRICA",

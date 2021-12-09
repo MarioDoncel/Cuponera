492bd984363cc1b64c701247446a484b7f2da3ae
Mario Andres Doncel Neto <88mario.doncel@gmail.com>
2021-12-09 12:45:37 -0300

    text corrections

--- scriptIndex.js
+++ scriptIndex.js
@@ -13,7 +13,7 @@ howItWorks.addEventListener('click', () => {
         <div class="howItWorks">
             <div class="textModal">
                 <h4>USTED PAGA LA CUENTA EN EFECTIVO Y PODRÁ COMPLETAR SU PAGO CON UNO DE NUESTROS CUPONES EN CUALQUIERA DE LOS ESTABLECIMIENTOS HABILITADOS</h4>
-                <p>Siempre le ofrecemos un descuento, puede ser desde hasta 5%, 10% o 20%. Depende de la categoría <small>[Junior, Senior, Master]</small> de COMPRA MÍNIMA escogida por cada establecimiento habilitado. <br><br> Verificque la categoría que escogió cada establecimiento y <a href="./stores.html">descubra los establecimientos comerciales</a>  de su comuna que más valor le dan a su dinero aceptando nuestros cupones!</p>
+                <p>Siempre le ofrecemos un descuento, puede ser desde hasta 5%, 10% o 20%. Depende de la categoría <small>[Junior, Senior, Master]</small> de COMPRA MÍNIMA escogida por cada establecimiento habilitado. <br><br> Verifique la categoría que escogió cada establecimiento y <a href="./stores.html">descubra los establecimientos comerciales</a>  de su comuna que más valor le dan a su dinero aceptando nuestros cupones!</p>
                 <h4>LOS PUEDE BUSCAR USTED MISMO O RECIBIRLOS EN SU CASA EN LAS COMUNAS 6 Y 7</h4> 
                 <p>Si desea economizar dinero y vive en la Comuna 6 recíbalos en su casa, solicitas la colección completa ($20 $50 $100 $200 $500 $1.000) por mensaje a nuestra página y la recibes gratis en su casa, el costo de la entrega se la paga usted al entregador después de recibir todos los cupones. </p>
 
@@ -36,7 +36,7 @@ howUse.addEventListener('click', () => {
     <div class="modal">
         <div class="howItWorks">
             <div class="textModal">
-                <h4>Si Usted ya tiene los 6 (seis) cupones de $ 20 /50 /100 /200 /500 /1.000 le enseñamos como usarlos todos en la mejor categoría de “COMPRA MÍNIMA” posible para economizar hasta 20% sólo por pagar las cuentas en efectivo.</h4>
+                <h4>Aquí le enseñamos como economizar hasta $1.870,- usando los 6 cupones existentes ($ 20 /50 /100 /200 /500 /1.000) como si fueran dinero en la Verdulería, Panadería, etc.</h4>
                 <br>
                 <h5>¿Cómo hacerlo? </h5>
                 <p>Lo primero a hacer es solicitar los cupones y se los entregaremos en su domicilio (solo entregamos en las Comunas 6 y 7), enseguida escoja en cuál de los establecimientos comerciales habilitados realizará su compra</p>
